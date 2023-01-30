ae61197553d6a997490b2edafe050a2d79bf5360
JMVillota <jeffersonvillota@gmail.com>
2023-01-30 10:02:16 -0500

    cambios

---
+++ index.js
@@ -0,0 +1,32 @@
+//PACKAGES
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const dbConnectnoSql = require("./Conexiones/noSql")
+const dbConnectSql = require("./Conexiones/slq")
+const serverless = require("serverless-http");
+
+
+//MIDDLEWARES
+app.use(cors())
+app.use(express.json())
+    // app.use(express.static("storage"))
+    // app.use("/api", require("./routes"))
+
+
+//CONTROLLER
+// const index_controller = require('./controllers/alquiler')
+
+//ROUTES
+app.use(require('./routes/index'));
+
+//EJECUTION
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Tu server esta listo por el puerto ${port}`)
+})
+
+module.exports = app;
+module.exports.handler = serverless(app);
