cef48901245c943cf2009927d06f96bd300582b3
JMVillota <jeffersonvillota@gmail.com>
2023-01-30 11:31:03 -0500

    cambios

--- express/server.js
+++ express/server.js
@@ -17,7 +17,7 @@ router.post('/', (req, res) => res.json({ postBody: req.body }));
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router); // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, './dist/index.html')));
-app.use(require('../routes/index'));
+app.use(require('/.netlify/functions/index'));
 
 module.exports = app;
 module.exports.handler = serverless(app);
