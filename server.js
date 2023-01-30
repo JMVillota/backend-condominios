f8a559c26e61b71b92a85d73ba2c255f61fc10bf
JMVillota <jeffersonvillota@gmail.com>
2023-01-30 12:29:48 -0500

    cambios

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
+
+
+app.use(require('./routes/index'));
+
+
+module.exports.handler = serverless(app);
