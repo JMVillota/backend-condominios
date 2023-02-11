4e782d5c11b5a16b75a16a3e41553c018b09f3d2
JMVillota <jeffersonvillota@gmail.com>
2023-02-11 18:28:33 -0500

    cambios

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
-//PACKAGES
-const express = require("express")
-const cors = require("cors")
-const app = express()
+'use strict';
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
 require("./Conexiones/noSql")
-const dbConnectSql = require("./Conexiones/slq")
-
-//MIDDLEWARES
-app.use(cors())
-app.use(express.json())
-    // app.use(express.static("storage"))
-    // app.use("/api", require("./routes"))
 
 
-//CONTROLLER
-// const index_controller = require('./controllers/alquiler')
-
-//ROUTES
+app.use(bodyParser);
 app.use(require('./routes/index'));
 
 
