f3d42fc2531150ebb9531b07172478474ec8a790
JMVillota <jeffersonvillota@gmail.com>
2023-02-11 18:26:59 -0500

    cambios img 3

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-'use strict';
-const express = require('express');
-const serverless = require('serverless-http');
-const app = express();
-const bodyParser = require('body-parser');
+//PACKAGES
+const express = require("express")
+const cors = require("cors")
+const app = express()
+require("./Conexiones/noSql")
+const dbConnectSql = require("./Conexiones/slq")
 
-app.use(bodyParser);
+//MIDDLEWARES
+app.use(cors())
+app.use(express.json())
+    // app.use(express.static("storage"))
+    // app.use("/api", require("./routes"))
+
+
+//CONTROLLER
+// const index_controller = require('./controllers/alquiler')
+
+//ROUTES
 app.use(require('./routes/index'));
 
 
