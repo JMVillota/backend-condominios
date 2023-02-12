89cf24765f5029d3e9e596554713f57ee713d69a
JMVillota <jeffersonvillota@gmail.com>
2023-02-11 19:36:59 -0500

    cambios

--- Conexiones/noSql.js
+++ Conexiones/noSql.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
-
 mongoose.set("strictQuery", false);
-
-const DB_URI = "mongodb+srv://paquinatoau:MCwfpotYHIibxXnQ@cluster0.hwb4wuh.mongodb.net/dbCondominos?retryWrites=true&w=majority"
-mongoose.connect(DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+const connectDB = async() => {
+    try {
+        await mongoose.connect('mongodb+srv://paquinatoau:MCwfpotYHIibxXnQ@cluster0.hwb4wuh.mongodb.net/dbCondominos?retryWrites=true&w=majority')
+        console.log('mongodb connection established');
+    } catch (error) {
+        console.log(error)
+    }
+}
+connectDB()
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 
 //MIDDLEWARES
 app.use(cors())
+app.use(dbConnectnoSql)
 app.use(express.json())
 app.use(bodyParser);
 app.use(require('./routes/index'));
