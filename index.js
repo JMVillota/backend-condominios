2dc60a30efa2faf98b8780fc0832a94b82fd3177
JMVillota <jeffersonvillota@gmail.com>
2023-02-11 20:04:22 -0500

    hshahssa

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ require("./Conexiones/slq")
 //MIDDLEWARES
 app.use(cors())
 app.use(express.json())
-    // app.use(express.static("storage"))
-    // app.use("/api", require("./routes"))
+app.use(express.urlencoded({ extended: false }))
+
 
 
 //CONTROLLER
