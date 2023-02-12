66d6b8432735173eb15e1a921e2dd5b9a5a31bc1
JMVillota <jeffersonvillota@gmail.com>
2023-02-11 20:45:14 -0500

    al fin

--- Conexiones/noSql.js
+++ Conexiones/noSql.js
@@ -1,21 +1,8 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
-
+const MongoClient = require('mongodb').MongoClient;
 const DB_URI = "mongodb+srv://paquinatoau:MCwfpotYHIibxXnQ@cluster0.hwb4wuh.mongodb.net/dbCondominos?retryWrites=true&w=majority"
-
-const connectDB = async() => {
-    try {
-        await mongoose.connect(DB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-
-        console.log('MongoDB Connected...');
-    } catch (err) {
-        console.error(err.message);
-        // Exit process with failure
-        process.exit(1);
-    }
-};
-
-module.exports = connectDB;
+const client = new MongoClient(DB_URI, { useNewUrlParser: true });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
