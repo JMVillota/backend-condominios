8f53b1f7f64c054d3a9bbe0f1bca2c4d13d34086
JMVillota <jeffersonvillota@gmail.com>
2023-02-11 19:47:11 -0500

    funciona mrdssss 00122

--- Conexiones/noSql.js
+++ Conexiones/noSql.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose')
+
 mongoose.set("strictQuery", false);
-const connectDB = async() => {
-    try {
-        await mongoose.connect('mongodb+srv://paquinatoau:MCwfpotYHIibxXnQ@cluster0.hwb4wuh.mongodb.net/dbCondominos?retryWrites=true&w=majority')
-        console.log('mongodb connection established');
-    } catch (error) {
-        console.log(error)
+
+const DB_URI = "mongodb+srv://paquinatoau:MCwfpotYHIibxXnQ@cluster0.hwb4wuh.mongodb.net/dbCondominos?retryWrites=true&w=majority"
+const pool = mongoose.connect(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, res) => {
+    if (!err) {
+        console.log('**** CONEXION CORRECTA ****')
+    } else {
+        console.log('***** ERROR DE CONEXION ****')
     }
-}
-exports.db = connectDB
+});
+
+exports.db = pool
