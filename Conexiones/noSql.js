324a3cc61b129e3d618e8de2a97f5c0186058943
JMVillota <jeffersonvillota@gmail.com>
2023-02-11 20:22:07 -0500

    tyur

--- Conexiones/noSql.js
+++ Conexiones/noSql.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose')
 mongoose.set("strictQuery", false);
-const connectDB = async() => {
-    const DB_URI = "mongodb+srv://paquinatoau:MCwfpotYHIibxXnQ@cluster0.hwb4wuh.mongodb.net/dbCondominos?retryWrites=true&w=majority"
-    mongoose.connect(DB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }, (err, res) => {
-        if (!err) {
-            console.log('**** CONEXION CORRECTA ****')
-        } else {
-            console.log('***** ERROR DE CONEXION ****')
-        }
-    })
-}
+
+const DB_URI = "mongodb+srv://paquinatoau:MCwfpotYHIibxXnQ@cluster0.hwb4wuh.mongodb.net/dbCondominos?retryWrites=true&w=majority"
+const connectDB = mongoose.connect(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, res) => {
+    if (!err) {
+        console.log('**** CONEXION CORRECTA ****')
+    } else {
+        console.log('***** ERROR DE CONEXION ****')
+    }
+})
+
 exports.db = connectDB
