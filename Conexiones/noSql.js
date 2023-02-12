631d94b5344e872bc18c04c646d3d4685f17c5aa
JMVillota <jeffersonvillota@gmail.com>
2023-02-11 20:37:29 -0500

    cambios finales

--- Conexiones/noSql.js
+++ Conexiones/noSql.js
@@ -1,22 +1,16 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
+const mongoose = require('mongoose')
+mongoose.set("strictQuery", false);
 
 const DB_URI = "mongodb://mongo:to1rG4ZcGIGBs4JevL6F@containers-us-west-57.railway.app:7023"
-
-
-const connectDB = async() => {
-    try {
-        await mongoose.connect(DB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-
-        console.log('MongoDB Connected...');
-    } catch (err) {
-        console.error(err.message);
-        // Exit process with failure
-        process.exit(1);
+const connectDB = mongoose.connect(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, res) => {
+    if (!err) {
+        console.log('**** CONEXION CORRECTA ****')
+    } else {
+        console.log('***** ERROR DE CONEXION ****')
     }
-};
+})
 
-module.exports = connectDB;
+exports.db = connectDB
