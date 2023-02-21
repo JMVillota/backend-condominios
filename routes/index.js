df6868a67e6ee627fb7b34cf332922247f31cead
JMVillota <jeffersonvillota@gmail.com>
2023-02-21 01:00:20 -0500

    Cambios

--- routes/index.js
+++ routes/index.js
@@ -237,7 +237,7 @@ router.delete('/Persona/:per_id', deletePersona)
 // Residente CRUD
 router.post('/Residente', createResidente)
 router.get('/Residente/:per_id', getResidente)
-router.get('/Residente', checkAuth, checkRoleAuth(['Presidente']), getAllResidente)
+router.get('/Residente', getAllResidente)
 router.delete('/Residente/:res_id', deleteResidente)
 router.put('/Residente/:per_id', updateResidente)
 
